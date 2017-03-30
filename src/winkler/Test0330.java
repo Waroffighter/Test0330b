f6a781f942e2bf849be8c979db9292dce53b5c2c
Leo Winkler <lwinkler@student.tgm.ac.at>
2017-03-30 14:03:10 +0200

    Englisch

--- src/winkler/Test0330.java
+++ src/winkler/Test0330.java
@@ -5,7 +5,7 @@ public class Test0330 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Guten Tag Hr. Winkler !!!!");
+		System.out.println("Good morning Hr. Winkler !!!!");
 
 	}
 
