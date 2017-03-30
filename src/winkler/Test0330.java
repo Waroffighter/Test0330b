f526761cc6b0045149e0abb940a9f55bacea9ffb
Leo Winkler <lwinkler@student.tgm.ac.at>
2017-03-30 14:05:13 +0200

    Github Repository

--- src/winkler/Test0330.java
+++ src/winkler/Test0330.java
@@ -1,6 +1,10 @@
 package winkler;
 
 public class Test0330 {
+	
+	/*
+	 * 		git@github.com:Waroffighter/Test0330.git
+	 * */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
