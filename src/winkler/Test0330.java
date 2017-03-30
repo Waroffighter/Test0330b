8cb014138b183432efbbda067689cc15fd62862e
Leo Winkler <lwinkler@student.tgm.ac.at>
2017-03-30 14:00:33 +0200

    Basis

---
+++ src/winkler/Test0330.java
@@ -0,0 +1,12 @@
+package winkler;
+
+public class Test0330 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Guten Tag Hr. Winkler !!!!");
+
+	}
+
+}
